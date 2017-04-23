a83f27012172e6724c032f056a5bfdc79ccb269c
Zane Wang <parazane@gmail.com>
2017-04-23 17:27:33 +1000

    initial push

---
+++ SportsStore.Tests/Controller/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                   new Product {ProductID = 2, Name = "P2"},
+                   new Product {ProductID = 3, Name = "P3"},
+                   new Product {ProductID = 4, Name = "P4"},
+                   new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+
+        }
+    }
+}
---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int page = 1)
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
+    }
+}
