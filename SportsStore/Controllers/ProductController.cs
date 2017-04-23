99f4fd49a394855bb1c1793a158281a2e543785b
Zane Wang <parazane@gmail.com>
2017-04-23 18:28:33 +1000

    update ProductListViewModels, which wrap up the priducts and page info

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using SportsStore.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -18,9 +18,16 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult List(int page = 1)
-            => View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+            => View(new ProductsListViewModel {
+                Products = repository.Products.OrderBy(p => p.ProductID)
+                .Skip((page -1)*PageSize)
+                .Take(PageSize),
+                PageInfo = new PageInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count();
+                }
+            });
     }
 }
---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PageInfo PageInfo { get; set; }
+
+    }
+}
