103b0f2fa8e349c164240afb63c7febb170c0911
Zane Wang <parazane@gmail.com>
2017-04-24 19:12:03 +1000

    add category navigator

---
+++ SportsStore/Componets/NAvigationMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStore.Componets
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke() =>
+            View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+    }
+}
