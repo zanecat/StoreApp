45a6777500747b7e009d6067a2b3479f69f966d3
Zane Wang <parazane@gmail.com>
2017-04-27 13:15:12 +1000

    add fontawesome in bower. add shopping cart component on navbar

---
+++ SportsStore/Componets/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Componets
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
