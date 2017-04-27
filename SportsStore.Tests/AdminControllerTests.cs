be5ec2ae0812d133f1d34686141383136ac80133
Zane Wang <parazane@gmail.com>
2017-04-27 21:27:17 +1000

    add unit test for post edit method

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -5,6 +5,8 @@ using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
 namespace SportsStore.Tests
 {
     public class AdminControllerTests
@@ -69,6 +71,47 @@ namespace SportsStore.Tests
             Product result = GetViewModel<Product>(target.Edit(4));
             // Assert
             Assert.Null(result);
+        }
+
+        [Fact]
+        public void Can_Save_Valid_Changes()
+        {
+            // Arrange - create mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            // Arrange - create mock temp data
+            Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object)
+            {
+                TempData = tempData.Object
+            };
+            // Arrange - create a product
+            Product product = new Product { Name = "Test" };
+            // Act - try to save the product
+            IActionResult result = target.Edit(product);
+            // Assert - check that the repository was called
+            mock.Verify(m => m.SaveProduct(product));
+            // Assert - check the result type is a redirection
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", (result as RedirectToActionResult).ActionName);
+        }
+        [Fact]
+        public void Cannot_Save_Invalid_Changes()
+        {
+            // Arrange - create mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            // Arrange - create a product
+            Product product = new Product { Name = "Test" };
+            // Arrange - add an error to the model state
+            target.ModelState.AddModelError("error", "error");
+            // Act - try to save the product
+            IActionResult result = target.Edit(product);
+            // Assert - check that the repository was not called
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
+            // Assert - check the method result type
+            Assert.IsType<ViewResult>(result);
         }
         private T GetViewModel<T>(IActionResult result) where T : class
         {
