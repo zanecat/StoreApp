46c6faf7f757b7c974a878bb2e05f10bea6271a8
Zane Wang <parazane@gmail.com>
2017-04-27 20:39:31 +1000

    add admin controller, index page

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
