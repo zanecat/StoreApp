aa9355c53a9061e91e956e6b26f30e2286ae856d
Zane Wang <parazane@gmail.com>
2017-04-27 13:45:48 +1000

    add order model/controller. add checkout view

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Chechout() => View(new Order());
+    }
+}
